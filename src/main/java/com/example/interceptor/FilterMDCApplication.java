19bcc152b7de4a793071082bc248fcb4c4d1b3c1
YohanNayanajith <yohannayanajith40@gmail.com>
2023-05-11 01:03:07 +0530

    first commit

---
+++ src/main/java/com/example/interceptor/FilterMDCApplication.java
@@ -0,0 +1,13 @@
+package com.example.interceptor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilterMDCApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilterMDCApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/interceptor/filter/LogFilter.java
@@ -0,0 +1,39 @@
+package com.example.interceptor.filter;
+
+import org.apache.commons.lang3.StringUtils;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.UUID;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
+
+@Slf4j
+public class LogFilter implements Filter {
+    public static final String CORRELATION_ID_HEADER_KEY = "X-Correlation-Id";
+    public static final String CORRELATION_ID_VALUE = "correlationId";
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig);
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+        final String correlationId = StringUtils.isNotEmpty(httpServletRequest.getHeader(CORRELATION_ID_HEADER_KEY)) ?
+                httpServletRequest.getHeader(CORRELATION_ID_HEADER_KEY) : UUID.randomUUID().toString();
+
+        MDC.put(CORRELATION_ID_VALUE, correlationId);
+        MDC.put("username", "Yohan");
+        log.info("Correlation ID {}",correlationId);
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+        Filter.super.destroy();
+    }
+}
